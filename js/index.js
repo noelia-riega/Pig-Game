c1854a6a0421e3a40dad90394b0c9739239b00fe
Noelia <noeliariega@gmail.com>
2024-02-18 18:27:12 -0300

    js errors

--- js/index.js
+++ js/index.js
@@ -20,11 +20,18 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1.Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   //   2. Display dice
   diceEl.classList.remove('hidden');
@@ -38,11 +45,18 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //Switch to next player
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+// when a player hold the score
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. Check if player's score is >= 100
+  // Finish the game
+  // Switch to the next player
+  switchPlayer();
 });
